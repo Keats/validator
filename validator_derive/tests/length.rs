9cff4a3e7c272a58c00c4413453f5cef203cc064
Tuetuopay <tuetuopay@me.com>
2020-09-21 18:01:08 +0200

    derive/tests: Add length tests for Vec reference

--- validator_derive/tests/length.rs
+++ validator_derive/tests/length.rs
@@ -67,3 +67,27 @@ fn can_specify_message_for_length() {
     assert_eq!(errs["val"].len(), 1);
     assert_eq!(errs["val"][0].clone().message.unwrap(), "oops");
 }
+
+#[test]
+fn can_validate_ref_for_length() {
+    use serde_json::Value;
+
+    #[derive(Debug, Validate)]
+    struct TestStruct<'a> {
+        #[validate(length(min = 5, max = 10))]
+        val: &'a Vec<String>,
+    }
+
+    let strings = vec![String::new()];
+    let s = TestStruct { val: &strings };
+    let res = s.validate();
+    assert!(res.is_err());
+    let err = res.unwrap_err();
+    let errs = err.field_errors();
+    assert!(errs.contains_key("val"));
+    assert_eq!(errs["val"].len(), 1);
+    assert_eq!(errs["val"][0].code, "length");
+    assert_eq!(errs["val"][0].params["value"], Value::Array(vec![Value::String(String::new())]));
+    assert_eq!(errs["val"][0].params["min"], 5);
+    assert_eq!(errs["val"][0].params["max"], 10);
+}
