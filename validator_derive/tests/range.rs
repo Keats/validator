11dd9f5eabde8cd4336d754e91f4f723683d87e4
xFrednet <xFrednet@gmail.com>
2020-11-22 19:00:18 +0000

    Removed test print statement

--- validator_derive/tests/range.rs
+++ validator_derive/tests/range.rs
@@ -110,7 +110,6 @@ fn value_out_of_range_fails_validation_with_self_path() {
     let res = s.validate();
     assert!(res.is_err());
     let err = res.unwrap_err();
-    println!("{}", err);
     let errs = err.field_errors();
     assert!(errs.contains_key("val"));
     assert_eq!(errs["val"].len(), 1);
