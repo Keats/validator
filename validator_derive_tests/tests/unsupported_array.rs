abbd5acdfa4c0b31e2548191b52f21d4a0b89e5c
Walter Bonetti <bonettiw@amotus.ca>
2022-03-23 08:36:37 -0400

    validatior_derive: allow unsupported_field_type add more tests

--- validator_derive_tests/tests/unsupported_array.rs
+++ validator_derive_tests/tests/unsupported_array.rs
@@ -1,4 +1,11 @@
-use validator::Validate;
+use validator::{Validate, ValidationError};
+
+fn valid_custom_fn(arr: &[u8; 2]) -> Result<(), ValidationError> {
+    match arr[0] == 1 {
+        true => Ok(()),
+        false => Err(ValidationError::new("meh")),
+    }
+}
 
 #[test]
 fn can_validate_valid_email_with_unsupported_array() {
@@ -7,10 +14,47 @@ fn can_validate_valid_email_with_unsupported_array() {
         #[validate(email)]
         val: String,
         #[allow(dead_code)]
-        array: [u8; 32],
+        array: [u8; 2],
     }
 
-    let s = TestStruct { val: "bob@bob.com".to_string(), array: [0u8; 32] };
+    let s = TestStruct { val: "bob@bob.com".to_string(), array: [0u8; 2] };
 
     assert!(s.validate().is_ok());
+}
+
+#[test]
+fn can_validate_custom_with_unsupported_array() {
+    #[derive(Debug, Validate)]
+    struct TestStruct {
+        #[validate(email)]
+        val: String,
+        #[validate(custom = "valid_custom_fn")]
+        array: [u8; 2],
+    }
+
+    let s = TestStruct { val: "bob@bob.com".to_string(), array: [1u8, 1u8] };
+
+    assert!(s.validate().is_ok());
+}
+
+#[test]
+fn can_fail_custom_with_unsupported_array() {
+    #[derive(Debug, Validate)]
+    struct TestStruct {
+        #[validate(email)]
+        val: String,
+        #[validate(custom = "valid_custom_fn")]
+        array: [u8; 2],
+    }
+
+    let s = TestStruct { val: "bob@bob.com".to_string(), array: [0u8, 1u8] };
+
+    let res = s.validate();
+    assert!(res.is_err());
+    let err = res.unwrap_err();
+    let errs = err.field_errors();
+    assert!(errs.contains_key("array"));
+    assert_eq!(errs["array"].len(), 1);
+    assert_eq!(errs["array"][0].code, "meh");
+    assert_eq!(errs["array"][0].params["value"][0], 0);
 }
