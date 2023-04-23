58313ef284f5fde01abe2c35c1dfd2ca9bbdcc2f
Ivan GJ <122645806+ivan-gj@users.noreply.github.com>
2023-04-23 23:46:00 +0200

    Feature: Add `exclusive_min` and `exclusive_max` to `range` validation (#246)
    * feat(range): add exclusive minimum and exclusive maximum for `range` validation
    
    * test(range): add tests for `exc_min` and `exc_max` range validation
    
    * docs: add docs for `exc_min` and `exc_max` for `range` validation
    
    * chore: rename `exc_min`, `exc_max` to `exclusive_min`, `exclusive_max`
    
    * chore(validation.rs): get rid of `collide` function

--- validator_derive_tests/tests/run-pass/range.rs
+++ validator_derive_tests/tests/run-pass/range.rs
@@ -26,4 +26,29 @@ struct Test {
     s11: Option<u8>,
 }
 
+#[derive(Validate)]
+struct Test2 {
+    #[validate(range(exclusive_min = 1.1, max = 2.2))]
+    s: isize,
+    #[validate(range(min = 1, exclusive_max = 2))]
+    s2: usize,
+    #[validate(range(exclusive_min = 18, exclusive_max = 22))]
+    s3: i32,
+    #[validate(range(exclusive_min = 18, max = 22))]
+    s4: i64,
+    #[validate(range(min = 18, exclusive_max = 22))]
+    s5: u32,
+    #[validate(range(min = 18, exclusive_max = 22))]
+    s6: u64,
+    #[validate(range(exclusive_min = 18.1, max = 22))]
+    s7: i8,
+    #[validate(range(exclusive_min = 18.0, max = 22))]
+    s8: u8,
+    #[validate(range(exclusive_min = 18.0, exclusive_max = 22))]
+    s9: Option<u8>,
+    #[validate(range(exclusive_min = 18.0))]
+    s10: Option<u8>,
+    #[validate(range(exclusive_max = 18.0))]
+    s11: Option<u8>,
+}
 fn main() {}
