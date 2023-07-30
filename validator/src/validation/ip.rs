5ab61169aabc5f26b7074c505e8f9d8a7210703a
pintariching <tilen.pintaric99@gmail.com>
2023-07-30 20:42:49 +0200

    Add ValidateIp trait

--- validator/src/validation/ip.rs
+++ validator/src/validation/ip.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::fmt::Display;
 use std::net::IpAddr;
 use std::str::FromStr;
 
@@ -27,6 +28,29 @@ where
     T: Into<Cow<'a, str>>,
 {
     IpAddr::from_str(val.into().as_ref()).is_ok()
+}
+
+pub trait ValidateIp {
+    fn validate_ipv4(&self) -> bool;
+    fn validate_ipv6(&self) -> bool;
+    fn validate_ip(&self) -> bool;
+}
+
+impl<T> ValidateIp for T
+where
+    T: ToString,
+{
+    fn validate_ipv4(&self) -> bool {
+        IpAddr::from_str(&self.to_string()).map_or(false, |i| i.is_ipv4())
+    }
+
+    fn validate_ipv6(&self) -> bool {
+        IpAddr::from_str(&self.to_string()).map_or(false, |i| i.is_ipv6())
+    }
+
+    fn validate_ip(&self) -> bool {
+        IpAddr::from_str(&self.to_string()).is_ok()
+    }
 }
 
 #[cfg(test)]
