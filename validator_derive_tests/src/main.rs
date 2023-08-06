598f3c430fa3c92a285000d293544b89cff7a98e
pintariching <tilen.pintaric99@gmail.com>
2023-08-06 22:54:54 +0200

    Remove code used for experementing

--- validator_derive_tests/src/main.rs
+++ validator_derive_tests/src/main.rs
@@ -1,31 +1,3 @@
-fn custom_validation(value: String, arg: i32) -> Result<(), String> {
-    Ok(())
-}
-
-struct TestStruct {
-    value: String,
-}
-
-pub trait ValidateArgs {
-    fn validate<F>(&self, value_closure: F) -> Result<(), String>
-    where
-        F: Fn(String) -> Result<(), String>;
-}
-
-impl ValidateArgs for TestStruct {
-    fn validate<F>(&self, value_closure: F) -> Result<(), String>
-    where
-        F: Fn(String) -> Result<(), String>,
-    {
-        value_closure(self.value.clone())
-    }
-}
-
 fn main() {
-    let test = TestStruct { value: "Test".to_string() };
-    let custom_closure = |val| custom_validation(val, 123);
-
-    let res = test.validate(|v| custom_validation(v, 123));
-
     println!("Hello, world!");
 }
