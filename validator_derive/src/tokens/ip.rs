5dda688d0852b5828722e8c57ce6d9610b7534c4
pintariching <tilen.pintaric99@gmail.com>
2023-09-10 15:05:17 +0200

    Replace if else with match

--- validator_derive/src/tokens/ip.rs
+++ validator_derive/src/tokens/ip.rs
@@ -8,30 +8,27 @@ pub fn ip_tokens(ip: Ip, field_name: &Ident, field_name_str: &str) -> proc_macro
     let message = quote_message(ip.message);
     let code = quote_code(ip.code, "ip");
 
-    let version = if let (Some(v4), Some(v6)) = (ip.v4, ip.v6) {
-        if v4 && v6 {
-            quote!(validate_ip())
-        } else if v4 {
-            quote!(validate_ipv4())
-        } else if v6 {
-            quote!(validate_ipv6())
-        } else {
-            quote!(validate_ip())
+    let version = match (ip.v4, ip.v6) {
+        (Some(v4), Some(v6)) => match (v4, v6) {
+            (true, false) => quote!(validate_ipv4()),
+            (false, true) => quote!(validate_ipv6()),
+            _ => quote!(validate_ip()),
+        },
+        (Some(v4), None) => {
+            if v4 {
+                quote!(validate_ipv4())
+            } else {
+                quote!(validate_ip())
+            }
         }
-    } else if let (Some(v4), None) = (ip.v4, ip.v6) {
-        if v4 {
-            quote!(validate_ipv4())
-        } else {
-            quote!(validate_ip())
+        (None, Some(v6)) => {
+            if v6 {
+                quote!(validate_ipv6())
+            } else {
+                quote!(validate_ip())
+            }
         }
-    } else if let (None, Some(v6)) = (ip.v4, ip.v6) {
-        if v6 {
-            quote!(validate_ipv6())
-        } else {
-            quote!(validate_ip())
-        }
-    } else {
-        quote!(validate_ip())
+        _ => quote!(validate_ip()),
     };
 
     quote! {
