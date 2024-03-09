c3add38622d8fd4e47f9d07dda987358d0af6ec7
Vincent Prouillet <github@vincentprouillet.com>
2024-03-10 00:07:40 +0100

    Attempt to fix everything

--- validator_derive/src/tokens/required_nested.rs
+++ validator_derive/src/tokens/required_nested.rs
@@ -1,26 +1,26 @@
 use quote::quote;
+use syn::Ident;
 
 use crate::types::Required;
 use crate::utils::{quote_code, quote_message};
 
 pub fn required_nested_tokens(
     required: Required,
-    field_name: &proc_macro2::TokenStream,
+    field_name: &Ident,
     field_name_str: &str,
 ) -> proc_macro2::TokenStream {
     let message = quote_message(required.message);
     let code = quote_code(required.code, "required");
-    // TODO: fix me
     quote! {
-        if !#field_name.validate_required() {
+        if self.#field_name.validate_required() {
+            if let Some(ref _tmp) = self.#field_name {
+                errors.merge_self(#field_name_str, _tmp.validate());
+            }
+        } else {
             #code
             #message
-            err.add_param(::std::borrow::Cow::from("value"), &#field_name);
+            err.add_param(::std::borrow::Cow::from("value"), &self.#field_name);
             errors.add(#field_name_str, err);
-        }
-
-        if let Some(ref #field_name) = self.#field_name {
-            errors.merge_self(#field_name_str, #field_name.validate());
         }
     }
 }
