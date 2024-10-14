60e7d511a8cd4736eeda262f0fadbcfa422c988e
Peerat Vichivanives <pvichivanives@thehive.ai>
2024-10-14 12:20:52 -0700

    disable with all-features

--- validator_derive_tests/tests/compile_test.rs
+++ validator_derive_tests/tests/compile_test.rs
@@ -2,6 +2,7 @@
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
+    #[cfg(not(feature = "nightly"))]
     t.compile_fail("tests/compile-fail/**/*.rs");
     t.pass("tests/run-pass/**/*.rs");
 }
