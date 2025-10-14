6af0382de1e9a8af03932077c79d3d9a9ef4dd60
qyuzh <zhangqingyu@ft.tech>
2025-10-14 17:59:42 +0800

    fix: enhance nested validation to support absolute paths in struct fields

--- validator_derive/src/tokens/nested.rs
+++ validator_derive/src/tokens/nested.rs
@@ -1,11 +1,15 @@
 use quote::quote;
 
+use crate::CrateName;
+
 pub fn nested_tokens(
     field_name: &proc_macro2::TokenStream,
     field_name_str: &str,
+    crate_name: &CrateName,
 ) -> proc_macro2::TokenStream {
     quote! {
         if let std::collections::hash_map::Entry::Vacant(entry) = errors.0.entry(::std::borrow::Cow::Borrowed(#field_name_str)) {
+            use #crate_name::Validate;
             errors.merge_self(#field_name_str, (&#field_name).validate());
         }
     }
